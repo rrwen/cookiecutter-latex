b1b263ec71b61f6583ded56a0437f1c6db66bc1e
rwenite <rwenite@gmail.com>
2017-11-10 03:54:29 -0500

    Fix double .tex file

--- {{cookiecutter.template_name}}/make.sh
+++ {{cookiecutter.template_name}}/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-latex {{cookiecutter.tex_file}} -interaction=nonstopmode
+latex {{cookiecutter.tex_file}}.tex -interaction=nonstopmode
 bibtex {{cookiecutter.tex_file}}.aux
-latex wen2017_reviewparamoptml.tex -interaction=nonstopmode
-pdflatex {{cookiecutter.tex_file}} -interaction=nonstopmode
+latex {{cookiecutter.tex_file}}.tex -interaction=nonstopmode
+pdflatex {{cookiecutter.tex_file}}.tex -interaction=nonstopmode
