1486c1642b7c8a8d0b8fcafb960e9a4ff42514e8
rwenite <rwenite@gmail.com>
2017-11-10 17:46:14 -0500

    Add latex examples

--- {{cookiecutter.template_name}}/make.sh
+++ {{cookiecutter.template_name}}/make.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-latex {{cookiecutter.tex_file}} -interaction=nonstopmode -halt-on-error
+pdflatex {{cookiecutter.tex_file}} -interaction=nonstopmode -halt-on-error
 bibtex {{cookiecutter.tex_file}}
-latex {{cookiecutter.tex_file}} -interaction=nonstopmode -halt-on-error
+pdflatex {{cookiecutter.tex_file}} -interaction=nonstopmode -halt-on-error
 pdflatex {{cookiecutter.tex_file}} -interaction=nonstopmode -halt-on-error
