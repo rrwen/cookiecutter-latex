687499f38f6ec0186efca1cb4ea7ba0a3af3618f
rwenite <rwenite@gmail.com>
2017-11-10 05:43:29 -0500

    Remove aux make.sh

--- {{cookiecutter.template_name}}/make.bat
+++ {{cookiecutter.template_name}}/make.bat
@@ -1,4 +1,4 @@
 latex {{cookiecutter.tex_file}}.tex -output-directory=log -interaction=nonstopmode
-bibtex log/{{cookiecutter.tex_file}}.aux
+bibtex log/{{cookiecutter.tex_file}}
 latex {{cookiecutter.tex_file}}.tex -output-directory=log -interaction=nonstopmode
 pdflatex {{cookiecutter.tex_file}}.tex -aux-directory=log -interaction=nonstopmode
--- {{cookiecutter.template_name}}/make.sh
+++ {{cookiecutter.template_name}}/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 latex {{cookiecutter.tex_file}}.tex -interaction=nonstopmode
-bibtex {{cookiecutter.tex_file}}.aux
+bibtex {{cookiecutter.tex_file}}
 latex {{cookiecutter.tex_file}}.tex -interaction=nonstopmode
 pdflatex {{cookiecutter.tex_file}}.tex -interaction=nonstopmode
