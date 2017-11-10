aaab50a994d1cadc12ffb61ad263e658f496dba2
rwenite <rwenite@gmail.com>
2017-11-10 05:59:03 -0500

    Remove .tex from make

--- {{cookiecutter.template_name}}/make.sh
+++ {{cookiecutter.template_name}}/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-latex {{cookiecutter.tex_file}}.tex -interaction=nonstopmode
+latex {{cookiecutter.tex_file}} -interaction=nonstopmode
 bibtex {{cookiecutter.tex_file}}
-latex {{cookiecutter.tex_file}}.tex -interaction=nonstopmode
-pdflatex {{cookiecutter.tex_file}}.tex -interaction=nonstopmode
+latex {{cookiecutter.tex_file}} -interaction=nonstopmode
+pdflatex {{cookiecutter.tex_file}} -interaction=nonstopmode
