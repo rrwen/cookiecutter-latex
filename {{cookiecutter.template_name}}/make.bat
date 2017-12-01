563a114a43465f34f19abfe2d73eb7aa614e6b61
Richard Wen <rrwen.dev@gmail.com>
2017-11-30 19:43:30 -0500

    Update make.bat

--- {{cookiecutter.template_name}}/make.bat
+++ {{cookiecutter.template_name}}/make.bat
@@ -1,4 +1,4 @@
-pdflatex {{cookiecutter.tex_file}} -output-directory=log -interaction=nonstopmode -halt-on-error
+xelatex {{cookiecutter.tex_file}} -output-directory=log -interaction=nonstopmode -halt-on-error
 bibtex log/{{cookiecutter.tex_file}} 
-pdflatex {{cookiecutter.tex_file}}  -aux-directory=log -interaction=nonstopmode -halt-on-error
-pdflatex {{cookiecutter.tex_file}}  -aux-directory=log -interaction=nonstopmode -halt-on-error
+xelatex {{cookiecutter.tex_file}}  -aux-directory=log -interaction=nonstopmode -halt-on-error
+xelatex {{cookiecutter.tex_file}}  -aux-directory=log -interaction=nonstopmode -halt-on-error
