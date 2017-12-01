21a1208d09f8f3b698cc4476ffd2c3b77535170e
Richard Wen <rrwen.dev@gmail.com>
2017-11-30 19:43:45 -0500

    Update make.sh

--- {{cookiecutter.template_name}}/make.sh
+++ {{cookiecutter.template_name}}/make.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pdflatex {{cookiecutter.tex_file}} -interaction=nonstopmode -halt-on-error
+xelatex {{cookiecutter.tex_file}} -interaction=nonstopmode -halt-on-error
 bibtex {{cookiecutter.tex_file}}
-pdflatex {{cookiecutter.tex_file}} -interaction=nonstopmode -halt-on-error
-pdflatex {{cookiecutter.tex_file}} -interaction=nonstopmode -halt-on-error
+xelatex {{cookiecutter.tex_file}} -interaction=nonstopmode -halt-on-error
+xelatex {{cookiecutter.tex_file}} -interaction=nonstopmode -halt-on-error
